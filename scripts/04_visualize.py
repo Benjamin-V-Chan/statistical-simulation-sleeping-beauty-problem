15a64917381fc821d1615748564817b5c039a4de
Benjamin Chan <benjamin.v.chan@gmail.com>
2025-02-10 23:56:57 -0800

    04 script outline

---
+++ scripts/04_visualize.py
@@ -0,0 +1,10 @@
+# 04_visualize_results.py
+
+# Import required libraries
+import matplotlib.pyplot as plt
+
+# Load summary statistics
+
+# Create a bar chart showing the number of awakenings for Heads and Tails
+
+# Save the visualization as an image file
